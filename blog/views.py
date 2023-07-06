4492e9ff7d99e6009114fa88136eba58df400645
Stuartpkd <stuartparkerdesign@gmail.com>
2023-07-06 14:39:26 +0000

    Fix issue on index page

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.view import generic
+from django.views import generic
 from .models import Post
 
 
-class Postlist(generic.ListView):
+class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
